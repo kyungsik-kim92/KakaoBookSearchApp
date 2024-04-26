358919e0a1df639512e6a2767c9b7141407ddaef
Kim Kyungsik <ksiki1@naver.com>
2024-04-26 09:22:22 +0900

    Retrofit Builder 추가

--- app/src/main/java/com/example/kakaobooksearchapp/network/BookApiService.kt
+++ app/src/main/java/com/example/kakaobooksearchapp/network/BookApiService.kt
@@ -1,8 +1,12 @@
 package com.example.kakaobooksearchapp.network
 
+import com.example.kakaobooksearchapp.constants.KakaoConstants
 import com.example.kakaobooksearchapp.constants.KakaoConstants.API_KEY
 import com.example.kakaopractice.network.response.BookSearchData
 import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
 import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.Query
@@ -17,5 +21,18 @@ interface BookApiService {
         @Query("page") page: Int,
         @Query("size") size: Int,
     ): Response<BookSearchData>
+
+
+    companion object{
+        fun create() : BookApiService{
+            return Retrofit.Builder()
+                .baseUrl(KakaoConstants.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(BookApiService::class.java)
+        }
+    }
+
+
 
 }
