37b4b0e11df719d5fbc2b75b76fa2aeb7120691f
Kim Kyungsik <ksiki1@naver.com>
2024-04-26 17:17:55 +0900

    TypeConverter 유틸 추가

---
+++ app/src/main/java/com/example/kakaobooksearchapp/util/TypeConverter.kt
@@ -0,0 +1,15 @@
+package com.example.kakaobooksearchapp.util
+
+import androidx.room.TypeConverter
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+
+@TypeConverter
+fun fromList(value: List<String>) = Json.encodeToString(value)
+
+@TypeConverter
+fun toList(value: String) = Json.decodeFromString<List<String>>(value)
+
+@TypeConverter
+fun fromString(value: String) = Json.decodeFromString<Int>(value)
