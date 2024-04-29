ed5e3537314d120dd77a0f7d043c8123b77a48e0
Kim Kyungsik <ksiki1@naver.com>
2024-04-29 15:58:14 +0900

    ViewModelFactory 수정

--- app/src/main/java/com/example/kakaobooksearchapp/util/BookInfoViewModelFactory.kt
+++ app/src/main/java/com/example/kakaobooksearchapp/util/BookInfoViewModelFactory.kt
@@ -2,15 +2,16 @@ package com.example.kakaobooksearchapp.util
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
 import com.example.kakaobooksearchapp.ui.bookinfo.BookInfoViewModel
 import com.example.kakaobooksearchapp.ui.bookmark.BookmarkRepository
 
 class BookInfoViewModelFactory(
-    private val savedStateHandle: SavedStateHandle,
-    private val bookMarkRepository: BookmarkRepository
+    private val bookmarkRepository: BookmarkRepository
 ) :
     androidx.lifecycle.ViewModelProvider.Factory {
-    override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        return BookInfoViewModel(savedStateHandle,bookMarkRepository) as T
+    override fun <T : ViewModel> create(modelClass: Class<T>,extras: CreationExtras): T {
+        return BookInfoViewModel(extras.createSavedStateHandle(),bookmarkRepository) as T
     }
 }
