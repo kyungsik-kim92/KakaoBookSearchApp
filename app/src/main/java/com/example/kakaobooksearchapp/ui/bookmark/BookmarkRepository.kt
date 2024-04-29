791303abbadd819c45170229dbb26bc172d7281e
Kim Kyungsik <ksiki1@naver.com>
2024-04-29 17:52:26 +0900

    BookmarkRepositoryImpl 추가

--- app/src/main/java/com/example/kakaobooksearchapp/ui/bookmark/BookmarkRepository.kt
+++ app/src/main/java/com/example/kakaobooksearchapp/ui/bookmark/BookmarkRepository.kt
@@ -3,17 +3,10 @@ package com.example.kakaobooksearchapp.ui.bookmark
 import com.example.kakaobooksearchapp.room.BookMarkItem
 import com.example.kakaobooksearchapp.room.BookSearchDao
 
-class BookmarkRepository(private var bookSearchDao: BookSearchDao) {
+interface BookmarkRepository {
 
-    suspend fun insertBook(item: BookMarkItem): Long =
-        bookSearchDao.insertBook(item)
-
-    suspend fun deleteBook(item: BookMarkItem): Int =
-        bookSearchDao.deleteBook(item)
-
-    fun getFavoriteBooks(): List<BookMarkItem> {
-        return bookSearchDao.getFavoriteBooks()
-    }
-
+    suspend fun insertBook(item: BookMarkItem): Long
+    suspend fun deleteBook(item: BookMarkItem): Int
+    fun getFavoriteBooks(): List<BookMarkItem>
 
 }
---
+++ app/src/main/java/com/example/kakaobooksearchapp/ui/bookmark/BookmarkRepositoryImpl.kt
@@ -0,0 +1,20 @@
+package com.example.kakaobooksearchapp.ui.bookmark
+
+import com.example.kakaobooksearchapp.room.BookMarkItem
+import com.example.kakaobooksearchapp.room.BookSearchDao
+import javax.inject.Inject
+
+class BookmarkRepositoryImpl @Inject constructor(private var bookSearchDao: BookSearchDao) :
+    BookmarkRepository {
+    override suspend fun insertBook(item: BookMarkItem): Long =
+        bookSearchDao.insertBook(item)
+
+    override suspend fun deleteBook(item: BookMarkItem): Int =
+        bookSearchDao.deleteBook(item)
+
+    override fun getFavoriteBooks(): List<BookMarkItem> {
+        return bookSearchDao.getFavoriteBooks()
+    }
+
+
+}
