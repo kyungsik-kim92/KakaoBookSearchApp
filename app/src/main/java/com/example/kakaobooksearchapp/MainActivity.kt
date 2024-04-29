f5fad4a1ca00f202642574b4aa5738c4eee3fbf7
Kim Kyungsik <ksiki1@naver.com>
2024-04-29 20:04:59 +0900

    MainViewModel,MainViewState 적용

--- app/src/main/java/com/example/kakaobooksearchapp/MainActivity.kt
+++ app/src/main/java/com/example/kakaobooksearchapp/MainActivity.kt
@@ -2,7 +2,12 @@ package com.example.kakaobooksearchapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.kakaobooksearchapp.base.BaseViewModel
+import com.example.kakaobooksearchapp.base.ViewState
+import com.example.kakaobooksearchapp.network.response.KakaoBookItem
 import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -11,4 +16,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
     }
+}
+
+@HiltViewModel
+class MainViewModel @Inject constructor() : BaseViewModel() {
+
+    fun addBookmark(item: KakaoBookItem) {
+        onChangedViewState(MainViewState.AddBookmark(item))
+    }
+
+    fun deleteBookmark(item: KakaoBookItem) {
+        onChangedViewState(MainViewState.DeleteBookmark(item))
+    }
+
+}
+
+
+sealed class MainViewState : ViewState {
+    data class AddBookmark(val item: KakaoBookItem) : MainViewState()
+    data class DeleteBookmark(val item: KakaoBookItem) : MainViewState()
 }
