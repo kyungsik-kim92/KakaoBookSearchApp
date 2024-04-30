5cb2fad986a9b3dae17b0ffe57d5087d8f7e779e
Kim Kyungsik <ksiki1@naver.com>
2024-05-01 07:28:14 +0900

    kakao Mapper 추가

---
+++ data/src/main/java/com/example/data/mapper/KakaoMapper.kt
@@ -0,0 +1,33 @@
+package com.example.data.mapper
+
+import com.example.data.api.response.BookSearchData
+import com.example.data.api.response.KakaoBookItem
+import com.example.data.api.response.Meta
+import com.example.domain.model.KakaoBook
+import com.example.domain.model.KakaoSearchBook
+import com.example.domain.model.KakaoSearchMeta
+
+fun KakaoBookItem.toKakaoBook() = KakaoBook(
+    authors,
+    contents,
+    datetime,
+    isbn,
+    price,
+    publisher,
+    salePrice,
+    status,
+    thumbnail,
+    title,
+    translators,
+    url
+)
+
+fun Meta.toKakaoSearchMeta() = KakaoSearchMeta(
+    isEnd, pageableCount, totalCount
+)
+
+
+fun BookSearchData.toKakaoSearchBook() = KakaoSearchBook(
+    kakaoBookItems.map { it.toKakaoBook() },
+    meta.toKakaoSearchMeta()
+)
