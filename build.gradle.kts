31f5e956e3bb38d1ba41a788d191f0e814418a65
Kim Kyungsik <ksiki1@naver.com>
2024-05-07 15:12:22 +0900

    domain module 생성

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,4 +6,5 @@ plugins {
     id ("org.jetbrains.kotlin.plugin.serialization") version "1.6.10" apply false
     id ("com.google.dagger.hilt.android") version "2.48.1" apply false
     id("com.google.devtools.ksp") version "1.9.22-1.0.16" apply false
+    alias(libs.plugins.androidLibrary) apply false
 }
