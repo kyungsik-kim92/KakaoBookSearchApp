b35021edd841491eb56cd37afdd476b259d564da
Kim Kyungsik <ksiki1@naver.com>
2024-05-07 16:39:05 +0900

    필요없는 파일 삭제

--- app/src/main/java/com/example/kakaobooksearchapp/constants/KakaoConstants.kt
+++
@@ -1,6 +0,0 @@
-package com.example.kakaobooksearchapp.constants
-
-object KakaoConstants {
-    const val BASE_URL = "https://dapi.kakao.com/"
-    const val API_KEY = "a9c9b825ab60749e299b375dc7b313e5"
-}
