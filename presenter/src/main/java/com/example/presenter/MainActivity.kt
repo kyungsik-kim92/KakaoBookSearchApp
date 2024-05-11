e19c9c85a06d43d5314298bc7d07c65fa9973b7e
Kim Kyungsik <ksiki1@naver.com>
2024-05-12 00:33:19 +0900

    MainActivity 수정

--- presenter/src/main/java/com/example/presenter/MainActivity.kt
+++ presenter/src/main/java/com/example/presenter/MainActivity.kt
@@ -1,38 +1,69 @@
 package com.example.presenter
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import com.example.presenter.base.BaseViewModel
-import com.example.presenter.base.ViewState
+import androidx.activity.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.viewModelScope
 import com.example.domain.model.KakaoBookmark
+import com.example.domain.usecase.DeleteBookmarkUseCase
+import com.example.domain.usecase.InsertBookmarkUseCase
+import com.example.presenter.base.BaseActivity
+import com.example.presenter.base.BaseViewModel
+import com.example.presenter.base.ViewEvent
+import com.example.presenter.base.ViewState
+import com.example.presenter.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+    private val mainViewModel by viewModels<MainViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainViewModel.viewEvent.map(::onChangedViewEvent).launchIn(lifecycleScope)
 
     }
+
+    private fun onChangedViewEvent(event: ViewEvent) {
+        when (event) {
+            is MainViewEvent.AddBookmark -> {
+                Snackbar.make(binding.root, "북마크가 추가 되었습니다.", Snackbar.LENGTH_SHORT).show()
+            }
+
+            is MainViewEvent.DeleteBookmark -> {
+                Snackbar.make(binding.root, "북마크가 해제 되었습니다.", Snackbar.LENGTH_SHORT).show()
+            }
+        }
+    }
+
 }
 
 @HiltViewModel
-class MainViewModel @Inject constructor() : BaseViewModel() {
+class MainViewModel @Inject constructor(
+    private val insertBookmarkUseCase: InsertBookmarkUseCase,
+    private val deleteBookmarkUseCase: DeleteBookmarkUseCase
+) : BaseViewModel() {
 
-    fun addBookmark(item: KakaoBookmark) {
-        onChangedViewState(MainViewState.AddBookmark(item))
+    fun addBookmark(item: KakaoBookmark) = viewModelScope.launch(Dispatchers.IO) {
+        insertBookmarkUseCase(item)
+        onChangedViewEvent(MainViewEvent.AddBookmark(item))
     }
 
-    fun deleteBookmark(item: KakaoBookmark) {
-        onChangedViewState(MainViewState.DeleteBookmark(item))
+    fun deleteBookmark(item: KakaoBookmark) = viewModelScope.launch(Dispatchers.IO) {
+        deleteBookmarkUseCase(item)
+        onChangedViewEvent(MainViewEvent.DeleteBookmark(item))
     }
-
 }
 
 
-sealed class MainViewState : ViewState {
-    data class AddBookmark(val item: KakaoBookmark) : MainViewState()
-    data class DeleteBookmark(val item: KakaoBookmark) : MainViewState()
+sealed class MainViewState : ViewState
+sealed interface MainViewEvent : ViewEvent {
+    data class AddBookmark(val item: KakaoBookmark) : MainViewEvent
+    data class DeleteBookmark(val item: KakaoBookmark) : MainViewEvent
 }
