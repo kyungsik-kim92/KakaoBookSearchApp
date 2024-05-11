edb78786ba88cc22ec63322cef1cbe1ce44e80f3
Kim Kyungsik <ksiki1@naver.com>
2024-05-11 23:12:26 +0900

    HomeViewState -> HomeViewEvent로 변경

--- presenter/src/main/java/com/example/presenter/ui/home/HomeViewState.kt
+++ presenter/src/main/java/com/example/presenter/ui/home/HomeViewState.kt
@@ -1,9 +1,12 @@
 package com.example.presenter.ui.home
 
 import com.example.domain.model.KakaoBook
+import com.example.presenter.base.ViewEvent
 import com.example.presenter.base.ViewState
 
-sealed class HomeViewState : ViewState {
-    data class RouteBookInfo(val item: KakaoBook) : HomeViewState()
+sealed class HomeViewState : ViewState
 
+
+sealed interface HomeViewEvent : ViewEvent {
+    data class RouteBookInfo(val item: KakaoBook) : HomeViewEvent
 }
