c4ca6b27fafc1d69a2c0efbca5becf74416824f3
Kim Kyungsik <ksiki1@naver.com>
2024-05-12 00:43:10 +0900

    BookInfoViewState -> Event로 변경

--- presenter/src/main/java/com/example/presenter/ui/bookinfo/BookInfoViewState.kt
+++ presenter/src/main/java/com/example/presenter/ui/bookinfo/BookInfoViewState.kt
@@ -3,11 +3,4 @@ package com.example.presenter.ui.bookinfo
 import com.example.domain.model.KakaoBookmark
 import com.example.presenter.base.ViewState
 
-sealed class BookInfoViewState : ViewState {
-
-    data class AddBookmarkResult(val result: Boolean, val item: KakaoBookmark) : BookInfoViewState()
-
-    data class DeleteBookmarkResult(val result: Boolean, val item: KakaoBookmark) :
-        BookInfoViewState()
-
-}
+sealed class BookInfoViewState : ViewState
