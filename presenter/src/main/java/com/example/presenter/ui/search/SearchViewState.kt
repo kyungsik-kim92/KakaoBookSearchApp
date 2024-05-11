8ad63e19a45185bd899ec8e1d452dd1bfbc9a5af
Kim Kyungsik <ksiki1@naver.com>
2024-05-12 00:27:32 +0900

    SearchViewState 수정

--- presenter/src/main/java/com/example/presenter/ui/search/SearchViewState.kt
+++ presenter/src/main/java/com/example/presenter/ui/search/SearchViewState.kt
@@ -1,12 +1,15 @@
 package com.example.presenter.ui.search
 
 import com.example.domain.model.KakaoBook
+import com.example.presenter.base.ViewEvent
 import com.example.presenter.base.ViewState
 
 sealed class SearchViewState : ViewState {
-
     data class GetSearchResult(val list: List<KakaoBook>) : SearchViewState()
-    data class AddBookmarkResult(val item: KakaoBook) : SearchViewState()
-    data class DeleteBookmarkResult(val item: KakaoBook) : SearchViewState()
-    data class isBookmark(val item: KakaoBook) : SearchViewState()
+}
+
+
+sealed interface SearchViewEvent : ViewEvent {
+    data class AddBookItem(val item : KakaoBook) : SearchViewEvent
+    data class DeleteBookItem(val item : KakaoBook) : SearchViewEvent
 }
