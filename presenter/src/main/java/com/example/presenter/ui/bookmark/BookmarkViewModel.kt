5f096e2746197bbb5e680bc90ee304485e964c21
Kim Kyungsik <ksiki1@naver.com>
2024-05-15 21:52:08 +0900

    BookmarkViewModel 의 Base 제거

--- presenter/src/main/java/com/example/presenter/ui/bookmark/BookmarkViewModel.kt
+++ presenter/src/main/java/com/example/presenter/ui/bookmark/BookmarkViewModel.kt
@@ -1,31 +1,23 @@
 package com.example.presenter.ui.bookmark
 
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleEventObserver
-import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.domain.usecase.GetFavoriteBookmarkUseCase
-import com.example.presenter.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
 class BookmarkViewModel @Inject constructor(private val getFavoriteBookmarkUseCase: GetFavoriteBookmarkUseCase) :
-    BaseViewModel(), LifecycleEventObserver {
+    ViewModel() {
 
-    override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
-        when (event) {
-            Lifecycle.Event.ON_RESUME -> {
-                getFavoriteBookmarkUseCase().map {
-                    onChangedViewEvent(BookmarkViewEvent.BookmarkResult(it))
-                }.launchIn(viewModelScope)
-            }
+    val bookmarkList = getFavoriteBookmarkUseCase().stateIn(
+        viewModelScope,
+        SharingStarted.WhileSubscribed(5_000),
+        emptyList()
+    )
 
-            else -> Unit
-        }
-    }
 }
 
 
