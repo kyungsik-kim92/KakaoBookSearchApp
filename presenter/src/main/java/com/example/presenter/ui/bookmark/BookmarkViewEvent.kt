56f768993e070eeadfe83d8ec9d19035e87e5d0d
Kim Kyungsik <ksiki1@naver.com>
2024-05-16 23:11:51 +0900

    BookmarkViewEvent 제거

--- presenter/src/main/java/com/example/presenter/ui/bookmark/BookmarkViewEvent.kt
+++
@@ -1,8 +0,0 @@
-package com.example.presenter.ui.bookmark
-
-import com.example.domain.model.KakaoBookmark
-import com.example.presenter.base.ViewEvent
-
-sealed class BookmarkViewEvent : ViewEvent {
-    data class BookmarkResult(val list : List<KakaoBookmark>): BookmarkViewEvent()
-}
