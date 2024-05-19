06c4e0d32823f17a5e0d5a78361cbeb16553fa13
Kim Kyungsik <ksiki1@naver.com>
2024-05-19 18:51:18 +0900

    필요없는 파일 제거

--- presenter/src/main/java/com/example/presenter/ui/bookinfo/BookInfoViewEvent.kt
+++
@@ -1,8 +0,0 @@
-package com.example.presenter.ui.bookinfo
-
-import com.example.presenter.base.ViewEvent
-
-sealed interface BookInfoViewEvent : ViewEvent {
-    data object AddBookmark : BookInfoViewEvent
-    data object DeleteBookmark : BookInfoViewEvent
-}
